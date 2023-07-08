a7a5a18ceda568b7e7da3e1f3e67542ef30e6b4c
seniorLikeToCode <abhishek234543@gmail.com>
2023-07-09 00:02:00 +0530

    dummy useContext added

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,3 +1,6 @@
+import { useStateContext } from "../Contexts/ContextProvider";
+
 export default function Dashboard() {
+    const { state, setState } = useStateContext(); // dummy state
     return <><h1>Dashboard</h1></>;
 }
