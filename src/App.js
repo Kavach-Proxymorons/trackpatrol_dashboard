39dee6cd454c3ede8c04225546344feabb30fa56
seniorLikeToCode <abhishek234543@gmail.com>
2023-07-17 16:37:04 +0530

    toaster added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useStateContext } from './Contexts/ContextProvider';
 import { Login, CreateBandobast, Dashboard, ManageAdmin, RegisterHardware, RegisterPersonnel, Setting } from './Pages';
@@ -9,23 +10,47 @@ function App() {
   const { user, auth, activeMenu } = useStateContext();
   return (
     <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 6000,
+          style: {
+            background: '#fff',
+            color: '#000',
+          },
 
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
       <div className='h-screen'>
         <Sidebar />
         <div className={activeMenu ? 'ml-72' : ''}>
           <Navbar />
-            <Routes >
-              <Route path='/login' element={<Login />} />
-              <Route path='/' exact element={<Dashboard />} />
-              <Route path='/dashboard' element={<Dashboard />} />
-              <Route path='/register/personnel' element={<RegisterPersonnel />} />
-              <Route path='/register/hardware' element={<RegisterHardware />} />
-              <Route path='/create/bandobast' element={<CreateBandobast />} />
-              <Route path='/manage/admin' element={<ManageAdmin />} />
-              <Route path='/setting' element={<Setting />} />
-              {/* <Route path='/logout' element={<Login />} /> */}
-              <Route path='*' element={<Dashboard />} />
-            </Routes >
+          <Routes >
+            <Route path='/login' element={<Login />} />
+            <Route path='/' exact element={<Dashboard />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/register/personnel' element={<RegisterPersonnel />} />
+            <Route path='/register/hardware' element={<RegisterHardware />} />
+            <Route path='/create/bandobast' element={<CreateBandobast />} />
+            <Route path='/manage/admin' element={<ManageAdmin />} />
+            <Route path='/setting' element={<Setting />} />
+            {/* <Route path='/logout' element={<Login />} /> */}
+            <Route path='*' element={<Dashboard />} />
+          </Routes >
         </div>
       </div>
     </>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -35,43 +35,48 @@ export default function Sidebar() {
     return <>
         <div className={`md:overflow-hidden overflow-auto md:hover:overflow-auto fixed ${activeMenu ? 'shadow-xl w-72' : ''}`}>
             {activeMenu &&
-                <div className='h-screen drop-shadow-md'>
-                    <Logo />
-                    <div className='bg-[#DBDBDB] w-full h-[2px] my-6'></div>
-                    <div className='flex flex-col gap-y-1 ml-6 mt-20 mr-6 '>
-                        <NavLink to='/'>
-                            <BiSolidDashboard size={25} />
-                            <span >Dashboard</span>
-                        </NavLink>
-
-                        <NavLink to='/register/personnel'>
-                            <HiUsers size={25} />
-                            <span >Register Personnel</span>
-                        </NavLink>
-                        <NavLink to='/create/bandobast'>
-                            <BsFillCarFrontFill size={25} />
-                            <span >Create Bandobast</span>
-                        </NavLink>
-                        <NavLink to='/register/hardware'>
-                            <HiMiniCpuChip size={25} />
-                            <span >Register Hardware</span>
-                        </NavLink>
+                <div className='flex flex-col justify-between h-screen drop-shadow-md'>
+                    <div className=''>
+                        <Logo />
                         <div className='bg-[#DBDBDB] w-full h-[2px] my-6'></div>
+                        <div className='flex flex-col gap-y-1 ml-6 mt-20 mr-6 '>
+                            <NavLink to='/'>
+                                <BiSolidDashboard size={25} />
+                                <span >Dashboard</span>
+                            </NavLink>
 
-                        <NavLink className='justify-between ' to='/setting'>
-                            <span >SETTINGS</span>
-                            <AiOutlineSetting size={25} />
-                        </NavLink>
-                        <NavLink to='/manage/admin'>
-                            <MdAdminPanelSettings size={25} />
-                            <span>Manage Admin</span>
-                        </NavLink>
-                        <NavLink to='/logout'>
-                            <IoLogOut size={25} />
-                            <span>Logout</span>
-                        </NavLink>
+                            <NavLink to='/register/personnel'>
+                                <HiUsers size={25} />
+                                <span >Register Personnel</span>
+                            </NavLink>
+                            <NavLink to='/create/bandobast'>
+                                <BsFillCarFrontFill size={25} />
+                                <span >Create Bandobast</span>
+                            </NavLink>
+                            <NavLink to='/register/hardware'>
+                                <HiMiniCpuChip size={25} />
+                                <span >Register Hardware</span>
+                            </NavLink>
+                            <div className='bg-[#DBDBDB] w-full h-[2px] my-6'></div>
+
+                            
+                            <NavLink to='/manage/admin'>
+                                <MdAdminPanelSettings size={25} />
+                                <span>Manage Admin</span>
+                            </NavLink>
+
+                        </div>
                     </div>
-
+                    <div className=' ml-6 mr-6 mb-4'> 
+                    <NavLink className='' to='/setting'>
+                                <AiOutlineSetting size={25} />
+                                <span >SETTINGS</span>
+                            </NavLink>
+                    <NavLink to='/logout'>
+                        <IoLogOut size={25} />
+                        <span>Logout</span>
+                    </NavLink>
+                    </div>
                 </div>
             }
         </div>
