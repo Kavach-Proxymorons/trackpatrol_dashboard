0b8906913772b6f3d124db8f070ccedf03b05c86
unknown <shahbaz_ali@outlook.in>
2023-08-04 18:24:11 +0530

    create table for attaching or removing hardware

--- src/Pages/Dashboard/Monitor/hardwarelist.jsx
+++ src/Pages/Dashboard/Monitor/hardwarelist.jsx
@@ -1,37 +1,29 @@
 import { useEffect, useState } from "react";
-import DataTable from "../../../Components/ui-components/dataTable";
-import { headers } from "../../../Components/ui-components/dataTable/skeletons/hardware";
+import DataTable from "../../../Components/ui-components/hardwareAllotmentTable";
+import { headers } from "../../../Components/ui-components/hardwareAllotmentTable/skeletons/hardware";
 import { useStateContext } from "../../../Contexts/ContextProvider";
 
-
-export default function HardwareList() {
-  const {hardwares, getHardwares} = useStateContext();
-  const [isOpen, setIsOpen] = useState(false);
+export default function HardwareList(props) {
+  const { hardwares, getHardwares, assignedAndIdleHardwares, getAssignedAndIdleHardwares } = useStateContext(); // to remove
+  const { data, shift_id } = props; // assigned hardware array from shift
+  const [tableData, setTableData] = useState([]); // for storing hardwares to display in table
+  const [toggleRerender, setToggleRerender] = useState(false); // to rerender the table after assigning hardware
 
   useEffect(() => {
     document.title = "Hardware's List | Bandobast";
-    // getHardwares();
-    // console.log(hardwares.hardwares_attached);
+    getAssignedAndIdleHardwares(shift_id);
   }, []);
 
   return (
-    <div className="mt-6 mr-12 border-2 rounded-md">
-      <h2
-        className="py-1 px-3 text-2xl font-medium text-slate-700 bg-slate-100 hover:bg-slate-200 rounded-t-md cursor-pointer"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        Hardware's List
-      </h2>
-      {isOpen && (
-        <div className="px-6">
-          <DataTable
+    <>
+      <h1 className="text-3xl mt-8 font-semibold">Attached Hardwares</h1>
+      <DataTable
             columns={headers}
             path={"hardware"}
-            data={hardwares}
-            fetchData={getHardwares}
+            data={assignedAndIdleHardwares}
+            fetchData={getAssignedAndIdleHardwares}
+            shift_id={shift_id}
           />
-        </div>
-      )}
-    </div>
+    </>
   );
 }
