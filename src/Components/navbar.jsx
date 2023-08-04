37313451a838c3146fd5112000beb26bf1420041
Prateek Chaurasia <prateek18801@gmail.com>
2023-08-04 16:48:43 +0530

    Use AuthContext

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,20 +1,21 @@
-import { useStateContext } from "../Contexts/ContextProvider";
+import { useContext } from "react";
 import { Profile } from ".";
+import AuthContext from "../Contexts/AuthContext";
 
 export default function Navbar() {
-  const { isLogged, activeMenu, name } = useStateContext();
-  return (
-    <>
-      {isLogged && (
-        <div className="sticky top-0 z-50 border-b-3 bg-background">
-            <div className="h-[78px] flex justify-between items-center ml-8">
-              <p className="text-xl text-neutral-900">
-                Hello <span className="font-medium">Admin</span>, Welcome back!
-              </p>
-              <Profile />
-            </div>
-        </div>
-      )}
-    </>
-  );
+    const { token } = useContext(AuthContext);
+    return (
+        <>
+            {token && (
+                <div className="sticky top-0 z-50 border-b-3 bg-background">
+                    <div className="h-[78px] flex justify-between items-center ml-8">
+                        <p className="text-xl text-neutral-900">
+                            Hello <span className="font-medium">Admin</span>, Welcome back!
+                        </p>
+                        <Profile />
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
