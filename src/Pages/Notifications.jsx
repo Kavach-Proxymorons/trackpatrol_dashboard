34beec2845cbf88a1fe86d21df264b5c3b67feef
Senior <abhishek234543@gmail.com>
2023-08-09 15:53:37 +0530

    notification css  done

--- src/Pages/Notifications.jsx
+++ src/Pages/Notifications.jsx
@@ -14,11 +14,14 @@ export default function Notifications() {
     const [version, setVersion] = useState(0);
     const { activeMenu } = useStateContext();
     const { isLoggedIn } = useContext(AuthContext);
-    const { response, error, loading } = useFetch("/api/v1/app/duty/getIssues", tid);
+    const { response, error, loading } = useFetch(
+        "/api/v1/app/duty/getIssues",
+        tid
+    );
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setVersion(v => v + 1);
+            setVersion((v) => v + 1);
         }, 7000);
         return () => clearInterval(interval);
     }, []);
@@ -29,29 +32,63 @@ export default function Notifications() {
         if (error) toast.error(error.message, { id: tid });
     }, [loading, error, response]);
 
-    const handleAttended = async () => {
-
-    }
+    const handleAttended = async () => {};
 
     return (
         <>
             {isLoggedIn && <Sidebar />}
-            <div className={`${isLoggedIn ? (activeMenu ? "ml-52" : "ml-[84px]") : ""} `}>
+            <div
+                className={`${
+                    isLoggedIn ? (activeMenu ? "ml-52" : "ml-[84px]") : ""
+                } `}
+            >
                 <Navbar />
                 <div className="">
                     {response?.data.map((notification) => (
-                        <div className="border-3 m-2 rounded-lg p-2 flex ">
+                        <div className="border m-2 rounded-lg p-2 grid grid-cols-4 justify-between items-center px-4 ">
                             <div>
                                 <div>Sid: {notification.issue_creator.sid}</div>
-                                <div>Creator: {notification.issue_creator.official_name} ({notification.issue_creator.designation})</div>
-                                <div>Category: {notification.issue_category}</div>
+                                <div>
+                                    Creator:{" "}
+                                    {notification.issue_creator.official_name} (
+                                    {notification.issue_creator.designation})
+                                </div>
+                                <div>
+                                    Category: {notification.issue_category}
+                                </div>
                                 <div>Created At: {notification.createdAt}</div>
                             </div>
                             {/* <div>Severity: High</div> */}
-                            <Badge variant={"outline"} className={`badgeHigh`}>Severe</Badge>
-                            {notification.issue_status === "pending" ? <Badge variant={"outline"} className={`badgeHigh`}>Pending</Badge> : <Badge variant={"outline"} className={`badgeHigh`}>Attended</Badge>}
-                            <div>Description: {notification.issue_description}</div>
-                            <Button onClick={handleAttended}>Mark Attended</Button>
+                            <div>
+                                Description: {notification.issue_description}
+                            </div>
+                            <div className="flex gap-x-4">
+                                <Badge
+                                    variant={"outline"}
+                                    className={`badgeHigh`}
+                                >
+                                    Severe
+                                </Badge>
+                                {notification.issue_status === "pending" ? (
+                                    <Badge
+                                        variant={"outline"}
+                                        className={`badgeHigh`}
+                                    >
+                                        Pending
+                                    </Badge>
+                                ) : (
+                                    <Badge
+                                        variant={"outline"}
+                                        className={`badgeHigh`}
+                                    >
+                                        Attended
+                                    </Badge>
+                                )}
+                            </div>
+
+                            <Button onClick={handleAttended}>
+                                Mark Attended
+                            </Button>
                         </div>
                     ))}
                 </div>
