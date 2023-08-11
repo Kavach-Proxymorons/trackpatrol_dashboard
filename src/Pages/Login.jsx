b07401d3f50bc09b4aaea8fcdc885891e8a3bd98
Senior <abhishek234543@gmail.com>
2023-08-11 19:48:37 +0530

    login page default sign up remove

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,93 +1,3 @@
-// import { useEffect, useState, useContext } from "react";
-// import logo from "../Assests/logo.png";
-// import image from "../Assests/Image4.png";
-// import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-// import Input from "../Components/ui-components/input";
-
-// export default function Login() {
-//     const { login } = useContext(AuthContext);
-
-//     const [eye, setEye] = useState(false);
-
-//     const handlerVisiblePassword = () => {
-//         const password = document.querySelector('input[name="password"]');
-//         if (password.type === "password") {
-//             password.type = "text";
-//             setEye(true);
-//         } else {
-//             password.type = "password";
-//             setEye(false);
-//         }
-//     };
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         const username = e.target.username.value;
-//         const password = e.target.password.value;
-//         login(username, password);
-//     };
-
-//     return (
-//         <>
-//             <div className="h-screen flex ">
-//                 <div className="w-1/2 bg-[#F4F6FA]">
-//                     {/* logo */}
-//                     <div className="">
-//                         <img className="m-12 " src={logo} />
-//                     </div>
-//                     {/* image */}
-
-//                     <div className="flex flex-col justify-between items-center h-auto">
-//                         <img className="w-[350px] drop-shadow-md" src={image} alt="image" />
-
-//                         {/* caption */}
-//                         <div className="mt-28 mx-32 drop-shadow-sm">
-//                             <h1 className="text-4xl mb-2 font-medium">
-//                                 Welcome to <span className="text-[#0D76D3]">Bandobast</span>
-//                             </h1>
-//                             <p className="text-lg text-[#7B7D92]">
-//                                 Create Bandobast venues and Duties. Monitor the location of
-//                                 police officers deployed on the Bandobast duty in real-time.
-//                             </p>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div className="ml-8 w-7/12  flex flex-col justify-center items-center drop-shadow">
-//                     <h1 className="text-4xl mb-20 font-medium text-center">Login</h1>
-//                     <form className="flex flex-col" onSubmit={handleSubmit}>
-//                         <label className="text-base font-medium">Username</label>
-//                         <Input
-//                             name="username"
-//                             className="h-12"
-//                             type="text"
-//                             placeholder="Username"
-//                         />
-//                         <label className="text-base font-medium mt-8">Password</label>
-//                         <div className="relative">
-//                             <Input
-//                                 name="password"
-//                                 className="h-12"
-//                                 type="password"
-//                                 placeholder="Password"
-//                             />
-//                             <div className="absolute right-0 bottom-3 mt-8 mr-8" onClick={handlerVisiblePassword}>
-//                                 {eye ? (
-//                                     <AiFillEye size={25} color="#0D76D3" />
-//                                 ) : (
-//                                     <AiFillEyeInvisible size={25} color="#0D76D3" />
-//                                 )}
-//                             </div>
-//                         </div>
-//                         <button className="bg-[#0D76D3] text-white rounded-lg mt-12 text-center py-4" type="submit">
-//                             Login
-//                         </button>
-//                     </form>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
 import { Link } from "react-router-dom";
 import { cn } from "../lib/utils";
 import { useState, useContext, useRef } from "react";
@@ -158,11 +68,6 @@ export default function AuthenticationPage() {
                         </svg>
                         KAVACH - TRACKPATROL
                     </div>
-
-                    {/* <div className="relative z-20 ">
-                        <p>KAVACH TRACKPATROL</p>
-                        <Asterisk size={500} className="opacity-30"/>
-                    </div> */}
                     <div className="relative z-20 flex justify-center items-center mt-auto opacity-40">
                         <svg className="spin spinner w-[250px] h-[250px]">
                             <path
@@ -172,10 +77,10 @@ export default function AuthenticationPage() {
                             <text className="text">
                                 <textPath
                                     href="#curve"
-                                    textLength={Math.floor(Math.PI * 2 * 98)}
+                                    textLength={Math.floor(Math.PI * 2 * 100)}
                                     className="text-xl"
                                 >
-                                    KAVACH - 2023 - TRACKPATROL -
+                                    KAVACH - 2023 - TRACKPATROL - 
                                 </textPath>
                             </text>
                         </svg>
@@ -198,7 +103,7 @@ export default function AuthenticationPage() {
                     <div className="flex w-full flex-col justify-center space-y-6 ">
                         <div className="flex flex-col space-y-2 text-center">
                             <h1 className="text-2xl font-semibold tracking-tight">
-                                Sign up to Trackpatrol
+                                Sign in to Trackpatrol
                             </h1>
                             <p className="text-sm text-muted-foreground">
                                 Enter your username and password below to
@@ -219,6 +124,7 @@ export default function AuthenticationPage() {
                                             name="username"
                                             placeholder="username"
                                             type="text"
+                                            value='SP_Ghaziabad'
                                             autoCapitalize="none"
                                             autoComplete="email"
                                             autoCorrect="off"
@@ -235,6 +141,7 @@ export default function AuthenticationPage() {
                                         <Input
                                             name="password"
                                             placeholder="some@1234"
+                                            value='admin@1234'
                                             type="password"
                                             autoCapitalize="none"
                                             ref={passwordRef}
@@ -263,7 +170,7 @@ export default function AuthenticationPage() {
                                     </Button>
                                 </div>
                             </form>
-                            <div className="relative">
+                            {/* <div className="relative">
                                 <div className="absolute inset-0 flex items-center">
                                     <span className="w-full border-t" />
                                 </div>
@@ -285,7 +192,7 @@ export default function AuthenticationPage() {
                                     <ArrowUpRight className="mr-2 h-4 w-4" />
                                 )}{" "}
                                 Default Login
-                            </Button>
+                            </Button> */}
                         </div>
                         <div className="px-8 text-center text-sm text-muted-foreground">
                             By clicking continue, you agree to our{" "}
